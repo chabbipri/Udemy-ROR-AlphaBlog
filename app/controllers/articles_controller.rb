baf9f4e3f53b9a1ea9aeb687ee50154958917858
pchabbi7 <pri.17feb@gmail.com>
2025-01-19 16:24:57 -0500

    Implementing DRY Concept by: 1. Adding common functions 2. Partials in the UI for the commonly used code.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit destroy update]
+
   def show
-    @article = Article.find_by(id: params[:id])
   end
 
   def index
@@ -12,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find_by(id: params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(permit_params)
     if @article.save
       flash[:notice] = "The Article was created Successfully."
       redirect_to @article
@@ -26,8 +26,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find_by(id: params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(permit_params)
       flash[:notice] = "The Article was successfully updated."
       redirect_to @article
     else
@@ -36,10 +35,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find_by(id: params[:id])
     @article.destroy
 
     flash[:alert] = "The Article was successfully deleted."
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def permit_params
+    params.require(:article).permit(:title, :description)
   end
 end
